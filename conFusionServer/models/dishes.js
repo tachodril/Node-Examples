e59338e912ee0178f1f10a6905865aab636686f0
tachodril <rajlevel97@gmai.com>
2019-12-21 19:22:48 +0530

    Integrate express and mongodb-1

--- conFusionServer/models/dishes.js
+++ conFusionServer/models/dishes.js
@@ -1,38 +1,56 @@
 const mongoose = require('mongoose');
 const schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
 
 var commentSchema = new schema({
     rating:  {
         type: Number,
         min: 1,
-        max: 5,
-        required: true
+        max: 5
+        //required: true
     },
     comment:  {
-        type: String,
-        required: true
+        type: String
+        //required: true
     },
     author:  {
-        type: String,
-        required: true
+        type: String
+        //required: true
     }
 }, {
     timestamps: true
 });
 
 const dishSchema = new schema({
-  name:{
-    type: String,
-    required:true,
-    unique: true
-  },
-  description:{
-    type: String,
-    required: true
-  },
-  comments:[commentSchema]
-},{
-  timestamps: true
+    name: {
+        type: String,
+        unique: true
+    },
+    description: {
+        type: String
+    },
+    image: {
+        type: String
+    },
+    category: {
+        type: String
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Currency,
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default:false
+    },
+    comments:[commentSchema]
+}, {
+    timestamps: true
 });
 
 var Dishes = mongoose.model('Dish',dishSchema);
