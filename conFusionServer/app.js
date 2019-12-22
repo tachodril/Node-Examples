84b9cf9f95c25dca09bc9d6346d146cbe1a9e320
tachodril <rajlevel97@gmai.com>
2019-12-23 00:02:22 +0530

    Express session login,signup,logout

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -45,54 +45,37 @@ app.use(session({
   store: new FileStore()
 }));
 
-function auth(req,res,next) {
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-  if(!req.session.user){
-    var authHeader = req.headers.authorization;
-    if(!authHeader) {
-      var err = new Error('You are not authenticated');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-      return;
-    }
+function auth (req, res, next) {
+    console.log(req.session);
 
-    var auth = new Buffer.from(authHeader.split(' ')[1],'base64')
-      .toString().split(':');
-    var username = auth[0];
-    var password = auth[1];
-    if(username == 'admin' && password == 'password'){
-      req.session.user = 'admin';
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-    }
+  if(!req.session.user) {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
   }
   else {
-    if(req.session.user === 'admin'){
+    if (req.session.user === 'authenticated') {
       next();
     }
     else {
-      var err = new Error('You are not authenticated');
-      err.status = 401;
-      next(err);
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
     }
   }
+}
 
 
   //console.log(req.headers);
 
-}
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
